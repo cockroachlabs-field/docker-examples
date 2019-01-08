ebc522ceba76a3a645f30b4636b6431353306c55
Tim Veil <tjveil@gmail.com>
2019-01-08 17:11:30 -0500

    updating echo

--- example-2/run.sh
+++ example-2/run.sh
@@ -2,13 +2,14 @@
 
 docker-compose up -d
 
-echo "waiting for source to initialize..."
+echo "waiting for cluster to come up..."
 sleep 20
 
 # configure license
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --execute="SET CLUSTER SETTING cluster.organization = 'xxx';"
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --execute="SET CLUSTER SETTING enterprise.license = 'xxx';"
 
+echo "waiting for license to be applied..."
 sleep 10
 
 # configure license
@@ -16,6 +17,7 @@ docker-compose exec roach-0 /cockroach/cockroach sql --insecure --execute="CREAT
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --database bank --execute="CREATE TABLE accounts (id INT PRIMARY KEY, balance DECIMAL);"
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --database bank --execute="INSERT INTO accounts VALUES (1, 1000.50);"
 
+echo "waiting for tables to be created..."
 sleep 10
 
 # execute backup
