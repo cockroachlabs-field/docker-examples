e3c68294c17da1a4d11a6f73e0730c5913c7ee88
Tim Veil <tjveil@gmail.com>
2019-01-14 14:34:54 -0500

    ycsb inprovements

--- example-3/ycsb/entrypoint.sh
+++ example-3/ycsb/entrypoint.sh
@@ -5,9 +5,12 @@ if [[ "${1-}" = "shell" ]]; then
     shift
     exec /bin/sh "$@"
 else
+    echo "entrypoint.sh:    creating table..."
     java -cp /opt/ycsb/lib/jdbc-binding.jar:/opt/ycsb/lib/postgresql-jdbc.jar com.yahoo.ycsb.db.JdbcDBCreateTable -P /opt/ycsb/conf/db.properties -n usertable
 
-    /opt/ycsb/bin/ycsb load jdbc -P /opt/ycsb/workloads/workloadb -P /opt/ycsb/conf/db.properties -p recordcount=20000
+    echo "entrypoint.sh:    loading data based on workload-cockroach..."
+    /opt/ycsb/bin/ycsb load jdbc -P /opt/ycsb/workloads/workload-cockroach -P /opt/ycsb/conf/db.properties
 
+    echo "entrypoint.sh:    executing [$@]"
     exec /opt/ycsb/bin/ycsb "$@"
 fi
