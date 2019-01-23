ec80945ddd647cb93de5e8773d58405e56a5b2b7
Tim Veil <tjveil@gmail.com>
2019-01-23 12:50:26 -0500

    got tpcc "working"

--- example-5/oltp-bench/entrypoint.sh
+++ example-5/oltp-bench/entrypoint.sh
@@ -8,14 +8,10 @@ if [[ "${1-}" = "shell" ]]; then
     exec /bin/sh "$@"
 else
 
-    tail -f /dev/null
+    echo "****** entrypoint.sh:    executing OLTP Benchmark: benchmark = [${BENCHMARK_NAME}] using config file = [${BENCHMARK_CONFIG_FILE}]..."
 
-#    echo "****** entrypoint.sh:    creating table..."
-#    java -cp /opt/ycsb/lib/jdbc-binding.jar:/opt/ycsb/lib/postgresql-jdbc.jar com.yahoo.ycsb.db.JdbcDBCreateTable -P /opt/ycsb/conf/db.properties -n usertable
-#
-#    echo "****** entrypoint.sh:    loading data based on ${WORKLOAD_NAME} with ${WORKLOAD_RECORD_COUNT} records..."
-#    /opt/ycsb/bin/ycsb load jdbc -P /opt/ycsb/workloads/${WORKLOAD_NAME} -P /opt/ycsb/conf/db.properties -p recordcount=${WORKLOAD_RECORD_COUNT}
-#
-#    echo "****** entrypoint.sh:    executing benchmark for workload ${WORKLOAD_NAME}, target OPS is ${WORKLOAD_TARGET_OPS}"
-#    exec /opt/ycsb/bin/ycsb run jdbc -target ${WORKLOAD_TARGET_OPS} -s -P /opt/ycsb/workloads//${WORKLOAD_NAME} -P /opt/ycsb/conf/db.properties
+    # don't like this hack cd; todo make this a bundled jar and remove clunky startup
+    cd  /opt/oltpbench
+    ./oltpbenchmark -b ${BENCHMARK_NAME} -c config/${BENCHMARK_CONFIG_FILE} --create=true --load=true --execute=true -s 5
+
 fi
