f983e051c8cfbb8190ef3b6cd6484b1585cd2220
Tim Veil <tjveil@gmail.com>
2019-04-08 11:21:52 -0400

    cleaning up run.sh

--- example-3/run.sh
+++ example-3/run.sh
@@ -2,17 +2,14 @@
 
 docker-compose build --no-cache
 
-## start source database and load balancer
 docker-compose up --no-start
 docker-compose start roach-0
 docker-compose start roach-1
 docker-compose start roach-2
 docker-compose start lb
 
-echo "waiting for cockroach to initialize..."
 sleep 5
 
-# create backup database
 docker-compose exec roach-0 /cockroach/cockroach sql --insecure --execute="CREATE DATABASE ycsb;"
 
 echo "starting ycsb..."
