531fc921580cbdf8d3d0d2caeb50ddc480cdd6f5
Tim Veil <tjveil@gmail.com>
2019-05-13 18:55:01 -0400

    adding additional executions

--- example-7/run.sh
+++ example-7/run.sh
@@ -2,3 +2,7 @@
 
 docker-compose build --no-cache
 docker-compose up -d
+
+docker-compose exec roach-0 /cockroach/cockroach sql --certs-dir=/mnt/certs/roach-0 --host=roach-0 --execute="CREATE DATABASE test;"
+docker-compose exec roach-0 /cockroach/cockroach sql --certs-dir=/mnt/certs/roach-0 --host=roach-0 --execute="CREATE USER test WITH PASSWORD 'password';"
+docker-compose exec roach-0 /cockroach/cockroach sql --certs-dir=/mnt/certs/roach-0 --host=roach-0 --execute="SET CLUSTER SETTING server.remote_debugging.mode = \"any\";"
