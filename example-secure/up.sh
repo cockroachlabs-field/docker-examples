ae6e0b2fd1b77cb032c7533ce23bbce5033d4b06
Tim Veil <3260845+timveil@users.noreply.github.com>
2021-03-22 21:17:41 -0400

    updating up.sh and compose for secure start... working well

--- example-secure/up.sh
+++ example-secure/up.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-docker-compose build --no-cache
-docker-compose up -d
+docker-compose up --no-start
+
+docker-compose start roach-cert
+
+sleep 5
+
+docker-compose start roach-0
+
+sleep 5
+
+docker-compose start roach-1
+docker-compose start roach-2
+docker-compose start lb
+
+sleep 5
+
+docker-compose start roach-init
