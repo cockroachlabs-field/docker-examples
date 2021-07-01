dbcbd4679e789fcf8c7ead5d28e6432abc570aba
Tim Veil <3260845+timveil@users.noreply.github.com>
2021-07-01 10:47:56 -0400

    updating with workload examples

--- example-haproxy-with-workload/up.sh
+++ example-haproxy-with-workload/up.sh
@@ -4,7 +4,10 @@ docker compose build --no-cache
 
 docker compose up -d
 
+sleep 30
+
+docker compose exec workload-client /cockroach/cockroach workload init --warehouses=1 tpcc "postgresql://root@lb:26257?sslmode=disable"
+
 sleep 10
 
-docker compose exec workload-client /cockroach/cockroach workload init --warehouses=3 tpcc "postgresql://root@lb:26257?sslmode=disable"
-docker compose exec workload-client /cockroach/cockroach workload run --warehouses=3 tpcc --tolerate-errors --duration=30m "postgresql://root@lb:26257?sslmode=disable"
+docker compose exec workload-client /cockroach/cockroach workload run --warehouses=1 tpcc --tolerate-errors --duration=30m "postgresql://root@lb:26257?sslmode=disable"
